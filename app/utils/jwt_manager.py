990589ec376540fa64b4af61b7ac9d5f1beaf8a9
Rudys Natanael Acosta <natanael926@gmail.com>
2023-09-18 23:43:21 -0400

    Add key_secret

--- app/utils/jwt_manager.py
+++ app/utils/jwt_manager.py
@@ -1,15 +1,17 @@
+import main
 from fastapi.encoders import jsonable_encoder
 from jwt import encode, decode
-from .serializer import custom_serializer
+
+algorithm = 'HS256'
 
 def create_token(data: dict):
     data = jsonable_encoder(data)
-    token: str = encode(payload=data, key='secret', algorithm='HS256')
+    token: str = encode(payload=data, key=main.KEY_SECRET, algorithm=algorithm)
     return token
 
 def validate_token(token: str)-> dict:
     try:
-        decoded_token: dict = decode(token, key='secret', algorithms='HS256') 
+        decoded_token: dict = decode(token, key=main.KEY_SECRET, algorithms=algorithm) 
         return decoded_token
     except:
         return None
--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 from fastapi import FastAPI 
-from app.routers import movie as movies_router, auth as auth_router, user as user_router
+from app.routers import movie as movies_router, \
+                auth as auth_router, user as user_router
 from config.database import engine, Base
 from app.middlewares.error_handler import ErrorHandler
+from fastapi.middleware.cors import CORSMiddleware
+
+KEY_SECRET = "SECRET"
 
 app = FastAPI()
 app.title = "My movie API"
 app.version = "0.0.1"
 
+origins = [
+    "http://localhost:8000",
+    "http://localhost:3000",
+    "http://localhost"
+]
+
 app.add_middleware(ErrorHandler)
+app.add_middleware(CORSMiddleware,
+                    allow_origins=origins,
+                    allow_credentials=True,
+                    allow_methods=["*"],
+                    allow_headers=["*"],)
 
 Base.metadata.create_all(bind=engine)
 
